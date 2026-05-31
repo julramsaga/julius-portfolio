bb343159a40112da30c6f173fc21d1f8b1b15ec2
Julram Saga <j.saga@jestopherit.ct.ws>
2026-05-31 22:15:13 +0800

    updated sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+// app/sitemap.js
+export default function sitemap() {
+    return [
+      {
+        url: 'https://julius-ramon-saga.vercel.app',
+        lastModified: new Date(),
+        priority: 1,
+      },
+    ]
+  }
