8f8cee0b8a44c421e733a55530b81d4bd8fa21b0
Julram Saga <j.saga@jestopherit.ct.ws>
2026-06-01 12:29:06 +0800

    robot

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+      rules: {
+        userAgent: '*',
+        allow: '/',
+      },
+      sitemap: 'https://julius-ramon-saga.vercel.app/sitemap.xml',
+    }
+  }
